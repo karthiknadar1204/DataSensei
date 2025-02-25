4edce1ebdf2cd35ab5e090a933507291e2532954
karthik <karthiknadar1204@gmail.com>
2025-02-25 13:18:42 +0530

    tiktoken fix

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,17 @@ const nextConfig = {
       bodySizeLimit: '3mb',
     },
   },
+  webpack: (config) => {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    };
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
